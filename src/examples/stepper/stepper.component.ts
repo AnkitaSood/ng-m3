3c6f06ac8ec69e7ee81c828d968559d87830f5ec
ankitasood <asood@octolabs.io>
2024-09-29 12:35:19 -0500

    feat: stepper

--- src/examples/stepper/stepper.component.ts
+++ src/examples/stepper/stepper.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, forwardRef} from '@angular/core';
+import {CdkStep, CdkStepper, CdkStepperModule} from "@angular/cdk/stepper";
+import {NgTemplateOutlet} from "@angular/common";
 
 @Component({
   standalone: true,
-  imports: [],
   templateUrl: './stepper.component.html',
-  styleUrl: './stepper.component.scss'
+  styleUrl: './stepper.component.scss',
+  imports: [forwardRef(() => CustomStepper), CdkStepperModule],
 })
-export class StepperComponent {
+export class StepperComponent {}
 
+/** Custom CDK stepper component */
+@Component({
+  selector: 'example-custom-stepper',
+  templateUrl: './example-custom-stepper.html',
+  styleUrl: './example-custom-stepper.scss',
+  providers: [{provide: CdkStepper, useExisting: CustomStepper}],
+  standalone: true,
+  imports: [NgTemplateOutlet, CdkStepperModule],
+})
+export class CustomStepper extends CdkStepper {
+  selectStepByIndex(index: number): void {
+    this.selectedIndex = index;
+  }
 }
